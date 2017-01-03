3d1764e348b915a375cc66874a374c8b90e2e177
Sojoner <hagentoennies@googlemail.com>
2017-01-03 10:20:05 +0100

    First version
    * basic stream processor
    * cli parsing
    * docker support

--- README.md
+++ README.md
@@ -1,20 +1,48 @@
 # clj-kstream-cutter
 
-A Clojure app to process one kafka topics. Select a .json
-field and split this into a list of token.
+A Clojure app to process one kafka topic. Select a .json
+field by providing a path like *root.substructure.fieldname* and split this into a list of token.
 
 ## Requirements
 
-* Your topics have .json values
+* [leiningen 2.7.1](https://leiningen.org/)
+* [kafka 0.10.0.1](http://kafka.apache.org) 
+* [docker 1.12.6](https://www.docker.com/)
 
-## Usage
+### Kafka Topic
+
+The topic you are consuming needs to have **^String** Keys and **^String** .json values.  
+
+## Build Clojure
+    
+    $lein check
+
+## Build .jar
+
+    $lein uberjar
+
+## Build .container
+    
+    $cd deploy
+    $./containerize.sh
+
+## Usage Leiningen
 
     $lein check
-    $lein run --broker kafka-broker:9092 --zookeeper zookeeper:2181 --input-topic logs-replay --output-topic mapped-test-json --selector msg --name stream-test
+    $lein run --broker kafka-broker:9092 --zookeeper zookeeper:2181 --input-topic logs-replay --output-topic mapped-test-json --selector msg --name stream-cut-json-field
+
+## Usage java
+
+    $java - jar clj-kstream-cutter.jar --broker kafka-broker:9092 --zookeeper zookeeper:2181 --input-topic logs-replay --output-topic mapped-test-json --selector msg --name stream-cut-json-field
+
+## Usage docker
+
+    $docker run -t -i <BUILD-HASH> --broker kafka-broker:9092 --zookeeper zookeeper:2181 --input-topic logs-replay --output-topic mapped-test-json --selector msg --name stream-cut-json-field
+
 
 ## License
 
-Copyright © 2016 Hagen Tönnies
+Copyright © 2017 Sojoner
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
--- src/clj_kstream_cutter/core.clj
+++ src/clj_kstream_cutter/core.clj
@@ -7,7 +7,7 @@
                                      StreamsConfig KeyValue)
            (org.apache.kafka.streams.kstream KStream
                                              KStreamBuilder
-                                             KTable KeyValueMapper ForeachAction)
+                                             KTable KeyValueMapper ForeachAction ValueMapper)
            (org.apache.kafka.streams.processor AbstractProcessor)
            (org.apache.kafka.common.serialization Deserializer
                                                   Serde
@@ -38,46 +38,40 @@
     (.put StreamsConfig/BOOTSTRAP_SERVERS_CONFIG (:kafka-brokers conf))
     (.put StreamsConfig/ZOOKEEPER_CONNECT_CONFIG (:zookeeper-servers conf))))
 
+(defn split-string-value-of-dict [data selector]
+  "Select a value given by the selector path, and split the string at the white space."
+  (try
+    (let [field-value (get-in data (into [] (first selector)))]
+      (map #(clojure.string/trim %1)
+           (clojure.string/split field-value #"\s")))
+    (catch Exception e
+      (error "Failed parsing field: " selector e)
+      (list))))
+
 (defn- stream-mapper
   "Main stream processor takes a configuration and a mapper function to apply."
-  [conf mapper-function]
+  [conf ]
   (let [streamBuilder (KStreamBuilder.)
         ^KStream log-stream (.stream
                               streamBuilder
                               stringSerde
                               stringSerde
-                              (into-array String [(:input-topic conf)]))
-        ^KStream mapped-stream (.map log-stream (reify KeyValueMapper
-                                                  (apply [this k v]
-                                                    (apply mapper-function k v (:selector conf)))))
-        ^KStream through (.through mapped-stream stringSerde stringSerde (:output-topic conf))
-        ^KafkaStreams streams (KafkaStreams. streamBuilder (get-props conf))]
-    (.start streams)))
+                              (into-array String [(:input-topic conf)]))]
+    (-> log-stream
+        (.flatMapValues (reify ValueMapper
+                          (apply [this value]
+                            (try
+                               (let [value-as-dict (json/read-str value :key-fn keyword)]
+                                 (split-string-value-of-dict value-as-dict (:selector conf) ))
+                               (catch Exception e
+                                 (error "Failed parsing .json" e)
+                                 (list))))))
+        (.map  (reify KeyValueMapper
+                 (apply [this k v]
+                   (KeyValue. v v))))
+        (.through stringSerde stringSerde (:output-topic conf)))
 
-
-(defn split-string-value-of-dict [data selector]
-  "Select a value given by the selector path, and split the string at the white space."
-  (try
-     (let [field-value (get-in data (into [] (first selector)))]
-       (info field-value)
-       (clojure.string/split field-value #"\s"))
-     (catch Exception e
-       (error "Failed parsing field: " selector e)
-       (list))))
-
-
-(defn process-item [key value selector]
-  "
-  Function that will be applied to every item in the stream.
-  * Read .json into .edn
-  * Process edn data
-  * Write serialize .edn back to .json
-  "
-  (info key value selector)
-  (let [value-as-dict (json/read-str value :key-fn keyword)
-        new-value (split-string-value-of-dict value-as-dict selector)]
-    (KeyValue. key (json/write-str new-value))))
-
+    (.start (KafkaStreams. streamBuilder (get-props conf)))))
 
 (defn -main [& args]
   (let [{:keys [options arguments errors summary]} (cli/parse-opts args cli-def/cli-options)
@@ -85,10 +79,10 @@
               :zookeeper-servers   (:zookeeper options)
               :input-topic (:input-topic options)
               :output-topic   (:output-topic options)
-              :selector (list (:selector options))
+              :selector (:selector options)
               :name (:name options)}]
     (cond
       (:help options) (cli-def/exit 0 (cli-def/usage summary))
       (not= (count (keys options)) 6) (cli-def/exit 1 (cli-def/usage summary))
       (not (nil? errors)) (cli-def/exit 1 (cli-def/error-msg errors)))
-    (stream-mapper conf process-item)))
+    (stream-mapper conf)))
