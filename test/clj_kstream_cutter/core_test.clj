4d5652ba65a4efdf8cfebd7ebc0a8db3540a4259
htonnies <htonnies@gaikai.com>
2017-05-22 13:05:19 +0200

    Remove test folder

--- test/clj_kstream_cutter/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clj-kstream-cutter.core-test
-  (:require [clojure.test :refer :all]
-            [clj-kstream-cutter.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
