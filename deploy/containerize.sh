e27ce35f649779cea750589aaae2ee9374608314
htonnies <htonnies@gaikai.com>
2017-05-22 16:51:11 +0200

    Fix typo

--- deploy/containerize.sh
+++ deploy/containerize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mv ../target/clj-kstream-cutter.jar .
 docker build --tag "sojoner/clj-kstream-cutter:0.3.0" .
-docker tag <HASH> sojoner/clj-kstream-cutter:0.3.0
-docker login
-docker push sojoner/clj-kstream-elasticsearch-sink
+#docker tag <HASH> sojoner/clj-kstream-cutter:0.3.0
+#docker login
+#docker push sojoner/clj-kstream-cutter
